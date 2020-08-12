3c322ca8f72509928f3c3a8ca4ae368a4de38b28
PrabhatKrRanjan <prabhatkrranjan95@gmail.com>
2020-08-12 22:59:13 +0530

    Update

--- index.js
+++ index.js
@@ -1,73 +1,91 @@
-// Getting Data From JSON File
-var movies
-fetch("data.json")
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        movies = data.movies
-        genres = data.genres
-        theatre = data.theatre
-        // location = data.location
+// Getting Recent 
 
-        console.log(movies)
+var movies
+fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=7ac617c2c72b5659bf38cc09855556f9")
+    .then(res => res.json())
+    .then((data) => {
+        movies = data.results
+        // console.log(movies)
         createCard(movies)
-        appendGenre(genres)
     })
     .catch((error) => {
         console.log(error)
     })
 
+// Movie Search Function
+
+function searchMovie() {
+    var searchName = document.getElementById("movieName").value;
+    console.log(searchName)
+
+    let url = "https://api.themoviedb.org/3/search/movie?api_key=7ac617c2c72b5659bf38cc09855556f9&language=en-US&query=" + searchName + "&page=1&include_adult=false"
+
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            movies = data.results
+            console.log(movies)
+            createCard(movies)
+        })
+        .catch((err) => console.log(err));
+}
+
 //Append Genre As Option
+
+fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=7ac617c2c72b5659bf38cc09855556f9&language=en-US")
+    .then(res => res.json())
+    .then(data => {
+        appendGenre(data.genres)
+        // console.log(data.genres)
+    })
+    .catch(err => console.log(err))
+
 function appendGenre(genres) {
     var addGenere = document.getElementById("addGenere");
     let output = ""
     for (let i = 0; i < genres.length; i++) {
-        output += `<option value=${genres[i].name}> ${genres[i].name}</option>`
+        output += `<option value=${genres[i].id}> ${genres[i].name}</option>`
     }
     addGenere.innerHTML = output;
 }
 
-// Genre
+// Movie by Genre 
+
 document.getElementById("addGenere").addEventListener("change", function () {
     var genereValue = document.getElementById('addGenere').value;
-    console.log("addGenere", genereValue)
+    console.log("Genere", genereValue)
 
-    if (genereValue == "All") {
-        createCard(movies)
-    }
-    else {
-        let genFilter = []
-        for (let i = 0; i < movies.length; i++) {
-            let gen = movies[i]["Genre"].split(",")
-            for (let j = 0; j < gen.length; j++) {
-                if (gen[j] == genereValue) {
-                    genFilter.push(movies[i])
-                }
-            }
-        }
-        if (genFilter.length == 0) {
-            document.getElementById("card").innerHTML = "<h1 class='text-white'>NO Movies</div>"
-        }
-        else {
-            createCard(genFilter)
-        }
-        console.log(genFilter)
-    }
+    let url = "https://api.themoviedb.org/3/discover/movie?api_key=7ac617c2c72b5659bf38cc09855556f9&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=" + genereValue
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            createCard(data.results)
+            // console.log(data.results)
+        })
+        .catch(err => console.log(err))
 })
 
 // Card Display Function
+
 function createCard(movies) {
     var card = document.getElementById("card");
     let output = ''
     for (let i = 0; i < movies.length; i++) {
+        let poster_path
+        if (movies[i].poster_path == null) {
+            poster_path = "https://dummyimage.com/300x450/000000/0011ff.png&text=Not+Found"
+        }
+        else {
+            poster_path = "https://image.tmdb.org/t/p/w300_and_h450_bestv2" + movies[i].poster_path
+        }
         output += `
-            <div class="card bg bg-dark col-sm-12 col-md-6 col-lg-3 p-4 mb-3">
-                <div class="text-center text-white">
-                <img class="cardImage" src="${movies[i].Poster}">
-                <h5>${movies[i].Title}</h5>
-                <div>Year - ${movies[i].Year}</div>
-                <a onclick="movieSelected('${movies[i].imdbID}')" class="btn btn-success" href="details.html">Book Now</a>
+            <div onclick="movieSelected('${movies[i].id}')" class="card bg bg-dark col-sm-12 col-md-6 col-lg-3 mb-5">
+                <img class="card-image-top" src="${poster_path}">
+                <div class="card-body text-white">
+                    <h6>${movies[i].title}</h6>
+                    <div>Release Date - ${movies[i].release_date}</div>
+                    <a onclick="movieSelected('${movies[i].id}')" class="btn btn-success" href="details.html">View</a>
                 </div>
             </div>
         `;
@@ -75,68 +93,86 @@ function createCard(movies) {
     }
 }
 
-// Movie Search Function
-function searchMovie() {
-    var searchName = document.getElementById("movieName").value;
-    console.log(searchName)
-    axios.get("https://www.omdbapi.com?apikey=5483a5bc&s=" + searchName)
-        .then(res => {
-            let movies = res.data.Search
-            console.log(movies)
-            createCard(movies)
-        })
-        .catch((err) => console.log(err));
-}
-
 // Setting Id To Session Storage
+
 function movieSelected(id) {
     sessionStorage.setItem('imdbId', id);
+    window.location.href = 'details.html';
 }
 
 // Getting Details of Setected Movie
-function getDetails() {
-    var imdbId = sessionStorage.getItem("imdbId");
-    var movieDetails = document.getElementById("movieDetails")
-    console.log(imdbId)
 
-    axios.get('https://www.omdbapi.com/?apikey=5483a5bc&i=' + imdbId)
+function getDetails() {
+    var movieId = sessionStorage.getItem("imdbId");
+    var movieDetails = document.getElementById("movieDetails")
+    let url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=7ac617c2c72b5659bf38cc09855556f9&language=en-US"
+    fetch(url)
+        .then(res => res.json())
         .then((response) => {
-            console.log(response);
-            let movie = response.data;
+            let movie = response;
+            console.log("movies", movie);
+            let poster_path;
+
+            if (movie.poster_path == null) {
+                poster_path = "https://dummyimage.com/300x450/000000/0011ff.png&text=Not+Found"
+            }
+            else {
+                poster_path = "https://image.tmdb.org/t/p/w300_and_h450_bestv2" + movie.poster_path
+            }
+
+            let genre = []
+            let genres = movie.genres;
+            for (let i = 0; i < genres.length; i++) {
+                genre.push(genres[i]["name"])
+            }
+            let language = []
+            let lang = movie.spoken_languages;
+            for (let i = 0; i < lang.length; i++) {
+                language.push(lang[i]["name"])
+            }
+
+            let production = [];
+            let company = movie.production_companies;
+            console.log(company)
+            for (let i = 0; i < company.length; i++) {
+                production.push(company[i]["name"])
+            }
 
             let output = `
-            <div class="row pt-5">
-                <div class="col-md-4">
-                    <div class="d-flex justify-content-center">
-                        <img src="${movie.Poster}">
+            <div class="row pt-5  mt-5 bg-linear">
+                <div class="col-sm-12 col-md-12 col-lg-4">
+                    <div class="d-flex justify-content-center mb-5">
+                        <img src="${poster_path}" alt= "${movie.poster_path}">
                     </div>
-                    <div class="text-success mt-4">
-                        <div> <span class="text-danger">Runtime - </span> ${movie.Runtime}</div>
-                        <div> <span class="text-danger">Gener - </span> ${movie.Genre}</div>
-                    </div>
+                   
                 </div>
-                <div class="col-md-8">
-                    <h1 class="text-white">${movie.Title}</h1>
+                <div class="col-sm-12 col-md-12 col-lg-8">
+                    <h1 class="text-white">${movie.original_title}</h1>
                     <ul class="list-group">
-                        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                        <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                        <li class="list-group-item"><strong class="text-danger">Genre : </strong> ${genre}</li>
+                        <li class="list-group-item"><strong class="text-danger">Language : </strong> ${language}</li>
+                        <li class="list-group-item"><strong class="text-danger">Rated : </strong> ${movie.vote_average}</li>
+                        <li class="list-group-item"><strong class="text-danger">Revenue : </strong> ${movie.revenue}</li>
+                        <li class="list-group-item"><strong class="text-danger">Release Date : </strong> ${movie.release_date}</li>
+                        <li class="list-group-item"><strong class="text-danger">Runtime : </strong> ${movie.runtime} Min </li>
+                        <li class="list-group-item"><strong class="text-danger">Production Companies : </strong> ${production}</li>
+                        <li class="list-group-item"><strong class="text-danger">Tagline : </strong> ${movie.tagline} </li>
                     </ul>
-                    <div class="text-white">
-                        <h3 class="pt-4">Plot</h3>
-                        <div>${movie.Plot}<div>
-                    </div>
+                    
                 </div>
+            </div>
+
+            <div class="">
+            <div class="text-white">
+                <h3 class="pt-4">Plot</h3>
+                <div>${movie.overview}<div>
             </div>
             <hr>
 
-            <div class=" row text-white">
-                <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-secondary ml-4">View IMDB</a>
-                <a href="index.html" class="btn btn-secondary ml-4">Go Back To Home</a>
+            <div class="row justify-content-center text-white">
+                <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-secondary ml-4">View IMDB</a>
+                <a href="${movie.homepage}" target="_blank" class="btn btn-secondary ml-4">Home Page</a>
+            </div>
             </div>
         `;
             movieDetails.innerHTML = output;
@@ -145,3 +181,17 @@ function getDetails() {
             console.log(err);
         });
 }
+
+// function displaySeat() {
+//     fetch("data.json")
+//         .then((res) => {
+//             return res.json()
+//         })
+//         .then((data) => {
+//             seats = data.seats
+//         })
+//         .catch((error) => {
+//             console.log(error)
+//         })
+//     console.log(seats)
+// }
